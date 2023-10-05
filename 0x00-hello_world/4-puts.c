1c9cfbc242db3bbeae307a063adb91a274ee8af7
limokipsang <limonoah1960@gmail.com>
2023-10-05 13:56:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,9 @@
  * RETURN: always 0
  */
 
-
 int main (void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
