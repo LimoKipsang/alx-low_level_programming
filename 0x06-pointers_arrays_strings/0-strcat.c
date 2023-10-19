ae659688728956846c49acfda0dfc9fefbb9aaaa
limokipsang <limonoah1960@gmail.com>
2023-10-19 16:48:20 -0700

    0strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@ int destlen = 0;
 int srclen = 0;
 int i;
 for (i = 0 ; dest[i] != '\0' ; i++)
-detlen++;
+destlen++;
 for (i = 0 ; src[i] != '\0' ; i++)
 srclen++;
 for (i = 0 ; i <= srclen ; i++)
