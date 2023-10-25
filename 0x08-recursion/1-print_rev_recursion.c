86b70bc49964b737a69b67b8648837bd22b26607
limokipsang <limonoah1960@gmail.com>
2023-10-25 12:40:49 -0700

    printrev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print strings in reverse
+ * @s: string to reverse
+ * Return: is 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
